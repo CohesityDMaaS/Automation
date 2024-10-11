a59f01bdf7dfb30295d76e7004665e14377353c2
altjohnson <112585381+altjohnson@users.noreply.github.com>
2024-10-11 13:00:14 -0400

    Update Protect_CCS_M365Sites.ps1

--- Protect_CCS_M365Sites/Protect_CCS_M365Sites.ps1
+++ Protect_CCS_M365Sites/Protect_CCS_M365Sites.ps1
@@ -1,181 +1,163 @@
-# Process command-line arguments
+# process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter()][string]$username = 'DMaaS',
-    [Parameter(Mandatory = $True)][string]$region,  # DMaaS region
-    [Parameter(Mandatory = $True)][string]$policyName,  # Protection policy name
-    [Parameter(Mandatory = $True)][string]$sourceName,  # Name of registered O365 source
-    [Parameter()][array]$sites,  # Optional names of mailboxes to protect
-    [Parameter()][string]$siteList,  # Optional text file of mailboxes to protect
-    [Parameter()][string]$startTime = '20:00',  # e.g. 23:30 for 11:30 PM
-    [Parameter()][string]$timeZone = 'America/New_York',  # e.g. 'America/New_York'
-    [Parameter()][int]$incrementalSlaMinutes = 60,  # Incremental SLA minutes
-    [Parameter()][int]$fullSlaMinutes = 120,  # Full SLA minutes
-    [Parameter()][int]$pageSize = 1000,
-    [Parameter()][bool]$wildcard  # If customer wants to search like terms and allows use of "*"
+    [Parameter()][string]$username = 'DMaaS', # Your CCS username (username@emaildomain.com)
+    [Parameter(Mandatory = $True)][string]$region,  # CCS region
+    [Parameter(Mandatory = $True)][string]$sourceName,  # name of registered O365 source
+    [Parameter()][array]$sites,  # optional names of SharePoint sites to unprotect
+    [Parameter()][string]$siteList = '',  # optional textfile of SharePoint sites to unprotect
+    [Parameter(Mandatory = $False)][bool]$deleteAllSnapshots = $false,  # whether all Snapshots are deleted (default to $False)
+    [Parameter()][int]$pageSize = 50000
 )
 
-# Gather list of sites to protect
+$outfileName = ".\log-Unprotect_CCS_M365Sites-$dateString.txt"
+
+# gather list of SharePoint sites to unprotect
 $sitesToAdd = @()
-
-if ($sites -ne "All") {
-    $sitesToAdd += $sites
-
-    if ($siteList) {
-        if (Test-Path -Path $siteList -PathType Leaf) {
-            $sitesToAdd += Get-Content $siteList
-        } else {
-            Write-Host "Site list $siteList not found!" -ForegroundColor Yellow
-            exit
+foreach($site in $sites){
+    $sitesToAdd += $site
+}
+if ('' -ne $siteList){
+    if(Test-Path -Path $siteList -PathType Leaf){
+        $sites = Get-Content $siteList
+        foreach($site in $sites){
+            $sitesToAdd += [string]$site
         }
-    }
-
-    # Remove empty entries
-    $sitesToAdd = $sitesToAdd | Where-Object { $_ -ne '' }
-
-    if ($sitesToAdd.Count -eq 0) {
-        Write-Host "No Sites specified" -ForegroundColor Yellow
+    }else{
+        Write-Host "Site list $siteList not found!" -ForegroundColor Yellow
         exit
     }
 }
 
-# Parse startTime
-if (-not ($startTime -match '^\d{1,2}:\d{2}$')) {
-    Write-Host "Please provide a valid start time" -ForegroundColor Yellow
+$sitesToAdd = @($sitesToAdd | Where-Object {$_ -ne ''})
+
+if($sitesToAdd.Count -eq 0){
+    Write-Host "No Sites specified" -ForegroundColor Yellow
     exit
 }
 
-$hour, $minute = $startTime.Split(':')
-
-# Source the cohesity-api helper code
+# source the cohesity-api helper code
 . $(Join-Path -Path .\ -ChildPath cohesity-api.ps1)
 
-# Authenticate
+# authenticate
 apiauth -username $username -regionid $region
 
-# Retrieve policy
-$policy = (api get -mcmv2 data-protect/policies?types=DMaaSPolicy).policies | Where-Object name -eq $policyName
-if (!$policy) {
-    Write-Host "Policy $policyName not found" -ForegroundColor Yellow
+$rootSource = api get protectionSources/rootNodes?environments=kO365 | Where-Object {$_.protectionSource.name -eq $sourceName}
+if(!$rootSource){
+    Write-Host "O365 Source $sourceName not found" -ForegroundColor Yellow
     exit
 }
 
-# Fetch sites and process based on parameters
+$source = api get "protectionSources?id=$($rootSource.protectionSource.id)&excludeOffice365Types=kMailbox,kUser,kGroup,kSite,kPublicFolder,kTeam,kO365Exchange,kO365OneDrive,kO365Sharepoint&allUnderHierarchy=false"
+$sitesNode = $source.nodes | Where-Object {$_.protectionSource.name -eq 'Sites'}
+if(!$sitesNode){
+    Write-Host "Source $sourceName is not configured for M365 Sites" -ForegroundColor Yellow
+    exit
+}
+
 $nameIndex = @{}
-$smtpIndex = @{}
-$userIds = @()
+#$smtpIndex = @{}
 
-if ($sites -ne "All" -and -not $wildcard) {
-    $rootSource = (api get protectionSources/rootNodes?environments=kO365) |
-                  Where-Object { $_.protectionSource.name -eq $sourceName }
+$users = api get "protectionSources?pageSize=$pageSize&nodeId=$($sitesNode.protectionSource.id)&id=$($sitesNode.protectionSource.id)&hasValidSites=true&allUnderHierarchy=false"
 
-    if (!$rootSource) {
-        Write-Host "O365 Source $sourceName not found" -ForegroundColor Yellow
-        exit
+while(1){
+    # implement pagination
+    foreach($node in $users.nodes){
+        $uniqueKey = "$($node.protectionSource.name)-$($node.protectionSource.id)"
+        $nameIndex[$uniqueKey] = $node.protectionSource.id
     }
-
-    $sitesNode = (api get "protectionSources?id=$($rootSource.protectionSource.id)&excludeOffice365Types=kMailbox,kUser,kGroup,kSite,kPublicFolder,kTeam,kO365Exchange,kO365OneDrive,kO365Sharepoint&allUnderHierarchy=false").nodes |
-                 Where-Object { $_.protectionSource.name -eq 'Sites' }
-
-    if (!$sitesNode) {
-        Write-Host "Source $sourceName is not configured for M365 Sites" -ForegroundColor Yellow
-        exit
-    }
-
-    $users = api get "protectionSources?pageSize=$pageSize&nodeId=$($sitesNode.protectionSource.id)&id=$($sitesNode.protectionSource.id)&hasValidSites=true&allUnderHierarchy=false"
-    while ($users) {
-        foreach ($node in $users.nodes) {
-            $nameIndex[$node.protectionSource.name] = $node.protectionSource.id
-        }
-        $cursor = $users.nodes[-1].protectionSource.id
-        $users = api get "protectionSources?pageSize=$pageSize&nodeId=$($sitesNode.protectionSource.id)&id=$($sitesNode.protectionSource.id)&hasValidSites=true&allUnderHierarchy=false&afterCursorEntityId=$cursor"
-        if (!$users.PSObject.Properties['nodes'] -or $users.nodes.Count -eq 1) {
-            break
-        }
-    }
-
-    foreach ($site in $sitesToAdd) {
-        $userId = if ($nameIndex.ContainsKey($site)) { $nameIndex[$site] } else { $smtpIndex[$site] }
-        if ($userId) {
-            $userIds += $userId
-        } else {
-            Write-Host "Site for $site not found" -ForegroundColor Yellow
-        }
+    $cursor = $users.nodes[-1].protectionSource.id
+    $users = api get "protectionSources?pageSize=$pageSize&nodeId=$($sitesNode.protectionSource.id)&id=$($sitesNode.protectionSource.id)&hasValidSites=true&allUnderHierarchy=false&afterCursorEntityId=$cursor"
+    if(!$users.PSObject.Properties['nodes'] -or $users.nodes.Count -eq 1){
+        break
     }
 }
-elseif ($wildcard) {
-    $protectedSources = (api get -v2 data-protect/search/protected-objects).objects |
-                        Where-Object objectType -eq "kSite"
 
-    if (!$protectedSources) {
-        Write-Host "No Sites found" -ForegroundColor Yellow
-        exit
-    }
+# find sites
+foreach($site in $sitesToAdd){
+    $matchSites = $nameIndex.Keys | Where-Object { $_ -like "*$site*" }
+    
+    if ($matchSites.Count -gt 0) {
+        foreach ($match in $matchSites) {
+            $userId = $nameIndex[$match]
 
-    foreach ($site in $sitesToAdd) {
-        $matchingSites = $protectedSources | Where-Object { $_.name -like "*$site*" }
-        if ($matchingSites) {
-            $userIds += $matchingSites.id
-            Write-Host "Site ID matching $site : $($matchingSites.id)"
-        }
-    }
-}
-else {
-    $protectedSources = (api get -v2 data-protect/search/protected-objects).objects |
-                        Where-Object objectType -eq "kSite"
+            # Attempt to get protection status for the site
+            try {
+                Write-Host "Checking protection status for $match..." -ForegroundColor Cyan
+                #$protectionStatus = api get -v2 "data-protect/protected-objects/$userId"
+                $protectionStatus = api get -v2 "data-protect/search/protected-objects?objectIds=$userId&objectActionKey=kO365Sharepoint"                
+                
+                if ($protectionStatus) {
+                    if ($protectionStatus.numResults -gt 0) {
+                        Write-Host "Unprotecting $match..." -ForegroundColor Green
+                        # Configure unprotection parameters
+                        $unProtectionParams = @{
+                            "action" = "UnProtect";
+                            "objectActionKey" = "kO365Sharepoint";
+                            "unProtectParams" = @{
+                                "objects" = @( 
+                                    @{
+                                        "id" = $userId;
+                                        "deleteAllSnapshots" = $deleteAllSnapshots;
+                                        "forceUnprotect" = $true;
+                                    };
+                                );
+                            };
+                        }
 
-    if (!$protectedSources) {
-        Write-Host "No Sites found" -ForegroundColor Yellow
-        exit
-    }
-
-    $userIds = $protectedSources | Where-Object { $_.latestSnapshotsInfo -eq 0 } | Select-Object -ExpandProperty id
-}
-
-# Configure protection parameters
-$protectionParams = @{
-    "policyId"         = $policy.id;
-    "startTime"        = @{
-        "hour"     = [int64]$hour;
-        "minute"   = [int64]$minute;
-        "timeZone" = $timeZone
-    };
-    "priority"         = "kMedium";
-    "sla"              = @(
-        @{
-            "backupRunType" = "kFull";
-            "slaMinutes"    = $fullSlaMinutes
-        };
-        @{
-            "backupRunType" = "kIncremental";
-            "slaMinutes"    = $incrementalSlaMinutes
-        }
-    );
-    "qosPolicy"        = "kBackupSSD";
-    "abortInBlackouts" = $false;
-    "objects"          = @()
-}
-
-# Protect the sites
-foreach ($userId in $userIds) {
-    if ($userId) {
-        $protectionParams.objects = @(@{
-            "environment"     = "kO365Sharepoint";
-            "office365Params" = @{
-                "objectProtectionType" = "kSharePoint";
-                "sharepointSiteObjectProtectionParams" = @{
-                    "objects"        = @(@{ "id" = $userId; "shouldAutoProtectObject" = $false });
-                    "indexingPolicy" = @{
-                        "enableIndexing" = $true;
-                        "includePaths"   = @("/");
-                        "excludePaths"   = @()
+                        # Unprotect objects
+                        $unprotectResponse = api post -v2 data-protect/protected-objects/actions $unProtectionParams 
+                        $unprotectResponse | out-file -filepath .\$outfileName -Append
+                        Write-Host "Unprotected $match" -ForegroundColor Green
+                    } else {
+                        Write-Host "$match is not currently protected, skipping unprotection." -ForegroundColor Yellow
                     }
+                } else {
+                    Write-Host "Protection status for $match not found, skipping." -ForegroundColor Red
                 }
             }
-        })
-        Write-Host "Protecting Site for ID $userId"
-        $response = api post -v2 data-protect/protected-objects $protectionParams
+            catch {
+                # Handle the case where protection status is not found (i.e., not protected)
+                Write-Host "Error fetching protection status for $match, skipping." -ForegroundColor Red
+            }
+        }
     } else {
-        Write-Host "ID not found" -ForegroundColor Yellow
+        Write-Host "Unable to find $site in order to unprotect." -ForegroundColor Yellow
     }
 }
+
+
+
+
+# find sites
+#foreach($site in $sitesToAdd){
+#    $matches = $nameIndex.Keys | Where-Object { $_ -like "*$site*" }
+#    if ($matches.Count -gt 0) {
+#        foreach ($match in $matches) {
+#            $userId = $nameIndex[$match]
+#            write-host "Unprotecting $match.." 
+
+            # configure unprotection parameters
+#            $unProtectionParams = @{
+#               "action" = "UnProtect";
+#                "objectActionKey" = "kO365Sharepoint";
+#                "unProtectParams" = @{
+#                    "objects" = @( 
+#                        @{
+#                            "id" = $userId;
+#                            "deleteAllSnapshots" = $deleteAllSnapshots;
+#                            "forceUnprotect" = $true;
+#                        };
+#                    );
+#                };
+#            }
+
+            # unprotect objects
+#           $unprotectResponse = api post -v2 data-protect/protected-objects/actions $unProtectionParams 
+#            $unprotectResponse | out-file -filepath .\$outfileName -Append
+#            Write-Host "Unprotected $match"
+#        }
+#    } else {
+#        Write-Host "Unable to Find $site in order to unprotect." 
+#    }
+#}
